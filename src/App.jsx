0f91f87a491856253ae86b5bd6cc387b8ec2970c
UriSmu <48240113@ort>
2025-05-29 10:24:50 -0300

    Pasar TP resuelto a repositorio nuevo

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Formulario from './Formulario';
 import Cita from './Cita';
 import Listado from './Listado';
 
 function App() {
+  const citasPorDefault = [{
+    id: 1,
+    mascota: "Shaina",
+    propietario: "Uriel Smucler",
+    fecha: "30/06",
+    hora: "10:20",
+    sintomas: "Control rutinario"
+  },
+  {
+    id: 2,
+    mascota: "Pluto",
+    propietario: "Mickey Mouse",
+    fecha: "26/06",
+    hora: "15:40",
+    sintomas: "Resfrío"
+  },
+  {
+    id: 3,
+    mascota: "Negrita",
+    propietario: "Pedro Gómez",
+    fecha: "22/05",
+    hora: "17:00",
+    sintomas: "Baño y corte higienico"
+  }]
+
   const [citas, setCitas] = useState([
-    {
-      id: 1,
-      mascota: "Shaina",
-      propietario: "Uriel Smucler",
-      fecha: "29/05",
-      hora: "10:20",
-      sintomas: "Control rutinario"
-    },
-    {
-      id: 2,
-      mascota: "Pluto",
-      propietario: "Mickey Mouse",
-      fecha: "26/05",
-      hora: "15:40",
-      sintomas: "Resfrío"
-    },
-    {
-      id: 3,
-      mascota: "Negrita",
-      propietario: "Pedro Gómez",
-      fecha: "22/05",
-      hora: "17:00",
-      sintomas: "Baño y corte higienico"
-    }
+    
   ]);
 
   const agregarCita = (nuevaCita) => {
@@ -38,9 +40,26 @@ function App() {
   const eliminarCita = (id) => {
     if(confirm("Seguro que desea eliminar la cita?"))
     {
-      setCitas(citas.filter(cita => cita.id !== id));
+      const nuevasCitas = citas.filter(cita => cita.id !== id);
+      setCitas(nuevasCitas);
+      localStorage.setItem('citas', JSON.stringify(nuevasCitas));
     }
   };
+
+  useEffect(() => {
+    const citasGuardadas = localStorage.getItem('citas');
+    if (citasGuardadas) {
+      setCitas(JSON.parse(citasGuardadas));
+    } else {
+      setCitas(citasPorDefault);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (citas?.length) {
+      localStorage.setItem('citas', JSON.stringify(citas));
+    }
+  }, [citas]);
 
   return (
     <>
